c14f2cbddeb04669b5a4502f32ca9533c63a2491
Kaio Gerhardt <gerhardt.kaio@gmail.com>
2023-05-08 22:24:02 -0300

    funcao de nome e criacao aleatoria dos objetos

--- desafio/src/PrincipalCandidatos.java
+++ desafio/src/PrincipalCandidatos.java
@@ -5,15 +5,20 @@ public class PrincipalCandidatos {
         Random random = new Random();
         int tamanho = random.nextInt(100) + 1;
 
-        String teste = getNome();
-        String novo = "teste";
+        Candidato[] candidatos = new Candidato[tamanho];
 
-        // Candidato[] candidatos = new Candidato[tamanho];
-        
+        for (int i = 0; i < tamanho; i++) {
+            String nome = randomNome();
+            String partido = randomPartido();
+            int intencoesVotos = random.nextInt(1000) + 1;
+            
+            candidatos[i] = new Candidato(nome, partido, intencoesVotos);
+        }
+
     }
 
     // metodo para gerar nome aleatorio
-    public static String getNome(){
+    public static String randomNome(){
         String[] nomes = {
             "João", "Maria", "Pedro", "Ana", "Paula", "Lucas", "Fernanda", "Rafael",
             "Camila", "Carlos", "Renata", "Marcelo", "Mariana", "Bruno", "Juliana",
@@ -30,6 +35,16 @@ public class PrincipalCandidatos {
         int nomeRandom = random.nextInt(nomes.length) + 1;
         int sobrenomeRandom = random.nextInt(sobrenomes.length) + 1;
 
-        return nomes[nomeRandom] + " " + sobrenomes[sobrenomeRandom];
+        return nomes[nomeRandom - 1] + " " + sobrenomes[sobrenomeRandom - 1];
+    }
+
+    // metodo que seleciona o partido aleatoriamente
+    public static String randomPartido(){
+        String[] partidos = {"PT", "PSDB", "PSB", "PMDB", "PV"};
+
+        Random random = new Random();
+        int partido = random.nextInt(partidos.length) + 1;
+
+        return partidos[partido - 1];
     }
 }
